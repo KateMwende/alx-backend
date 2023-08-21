7eb55c6df27781840c2d7c7d0b6164ec3de889b2
kate mwende <kmwende419@gmail.com>
2023-08-21 18:12:41 +0300

    Return tuple of indexes of a list

---
+++ 0x00-pagination/0-simple_helper_function.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+index_range function returns a tuple
+"""
+
+
+def index_range(page: int, page_size: int) -> float:
+    """Return tuple of start and end idx of a list"""
+    start_index = (page - 1) * page_size
+    end_index = page * page_size
+
+    return(start_index, end_index)
