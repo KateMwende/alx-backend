a2f363ee20924a7a638d01becbc8c2f0ec15c030
kate mwende <kmwende419@gmail.com>
2023-08-21 19:34:12 +0300

    Fixed pycodestyle

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -6,12 +6,13 @@ import csv
 import math
 from typing import List
 
-def index_range(page: int, page_size: int) -> float:
-        """Return tuple of start and end idx of a list"""
-        start_index = (page - 1) * page_size
-        end_index = page * page_size
 
-        return (start_index, end_index)
+def index_range(page: int, page_size: int) -> float:
+    """Return tuple of start and end idx of a list"""
+    start_index = (page - 1) * page_size
+    end_index = page * page_size
+
+    return (start_index, end_index)
 
 
 class Server:
@@ -19,10 +20,8 @@ class Server:
     """
     DATA_FILE = "Popular_Baby_Names.csv"
 
-
     def __init__(self):
         self.__dataset = None
-
 
     def dataset(self) -> List[List]:
         """Cached dataset
@@ -34,7 +33,6 @@ class Server:
                 self.__dataset = dataset[1:]
 
         return self.__dataset
-
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
         """Returns a list from a dataset in csv file"""
