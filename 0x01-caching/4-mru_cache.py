a67e10df2e80b6348e232af8f84dd5162d38078e
kate mwende <kmwende419@gmail.com>
2023-08-24 09:23:30 +0300

    Discard the most recently used in cache

---
+++ 0x01-caching/4-mru_cache.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+"""
+Least recently used algorithm
+"""
+from base_caching import BaseCaching
+from collections import deque
+
+
+class MRUCache(BaseCaching):
+    """Remove least recently used"""
+    def __init__(self):
+        """Initialize but with BaseCaching"""
+        super().__init__()
+        self.queue = deque()
+
+    def put(self, key, item):
+        """Used to discard key"""
+        if key is None or item is None:
+            return
+
+        if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+            while self.queue:
+                rec_used = self.queue.pop()
+                if rec_used in self.cache_data:
+                    del self.cache_data[rec_used]
+                    break
+
+            print("DISCARD: {}".format(rec_used))
+        self.queue.append(key)
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """Returns key in cached data"""
+        if key is None or self.cache_data.get(key) is None:
+            return None
+        self.queue.remove(key)
+        self.queue.append(key)
+        return self.cache_data[key]
