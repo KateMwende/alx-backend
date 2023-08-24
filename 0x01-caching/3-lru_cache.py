fe9ed36af1bca0e8db26aad42685f7661574c090
kate mwende <kmwende419@gmail.com>
2023-08-24 09:03:02 +0300

    Use LRU algorithm on cache data

---
+++ 0x01-caching/3-lru_cache.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+"""
+Least recently used algorithm
+"""
+from base_caching import BaseCaching
+from collections import deque
+
+
+class LRUCache(BaseCaching):
+    """Remove least recently used"""
+    def __init__(self):
+        """Initialize but with BaseCaching"""
+        super().__init__()
+        self.queue = deque()
+
+    def put(self, key, item):
+        """Used to discard key"""
+        if key is None or item is None:
+            return
+
+        if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+            while self.queue:
+                least_used = self.queue.popleft()
+                if least_used in self.cache_data:
+                    del self.cache_data[least_used]
+                    break
+
+            print("Discard: {}".format(least_used))
+        self.queue.append(key)
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """Returns key in cached data"""
+        if key is None or self.cache_data.get(key) is None:
+            return None
+        return self.cache_data[key]
