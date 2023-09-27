7bdb9eb786593d24ae2ba8d1e8667d5fcec814bc
Kate Mwende <Kmwende419@gmail.com>
2023-09-27 21:08:02 +0300

    Node Redis client and basic operations

---
+++ 0x03-queuing_system_in_js/1-redis_op.js
@@ -0,0 +1,30 @@
+import redis, { createClient } from 'redis';
+
+// Create a Redis client
+
+const client = createClient();
+
+// When the connection works
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
+
+// when the connection fails
+client.on('error', (err) => {
+  console.log('Redis client not connected to the server: ${err.message}');
+});
+
+
+function setNewSchool(schoolName, value) {
+  client.set(schoolName, value, redis.print);
+};
+
+function displaySchoolValue(schoolName) {
+  client.get(schoolName, (_err, reply) => {
+    console.log(reply);
+  });
+};
+
+displaySchoolValue('Holberton');
+setNewSchool('HolbertonSanFrancisco', '100');
+displaySchoolValue('HolbertonSanFrancisco');
