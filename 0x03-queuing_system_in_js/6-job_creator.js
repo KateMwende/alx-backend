40167d3507458a0bb37148e0f3fbc74ee4e29134
Kate Mwende <Kmwende419@gmail.com>
2023-09-29 21:19:58 +0300

    Created the Job creator with the queue

---
+++ 0x03-queuing_system_in_js/6-job_creator.js
@@ -0,0 +1,22 @@
+import { createQueue } from 'kue';
+
+const queue = createQueue();
+
+const jobData = {
+    phoneNumber: '07272',
+    message: 'code',
+};
+
+const job = queue.create('push_notification_code', jobData).save((err) => {
+  if (!err) {
+    console.log('Notification job created:', job.id);
+  }
+  });
+
+job.on('complete', () => {
+  console.log('Notification job completed');
+});
+
+job.on('failure', () => {
+  console.log('Notification job failed');
+});
