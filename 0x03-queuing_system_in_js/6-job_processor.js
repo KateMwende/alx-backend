871f744784ad0c0fa799f53274684ba6147b6e58
Kate Mwende <Kmwende419@gmail.com>
2023-10-03 18:33:04 +0300

    Creating a job

---
+++ 0x03-queuing_system_in_js/6-job_processor.js
@@ -0,0 +1,8 @@
+import kue, { createQueue } from 'kue';
+
+const queue = createQueue();
+
+function sendNotification(phoneNumber, message) {
+  console.log(`Sending notification to ${phoneNumber},
+  with message ${message}`);
+}
